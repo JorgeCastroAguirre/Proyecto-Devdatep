811782c0c7f6d6d19f8302066882da43086f51aa
JorgeCastroAguirre <JorgeCastroAguirre>
2026-03-18 03:12:13 -0500

    style: :lipstick: realize el cambio de estilo de mi skeleton
    Cambie la los colores y la forma de mi skeleton

--- src/components/PersonajeSkeleton.jsx
+++ src/components/PersonajeSkeleton.jsx
@@ -1,12 +1,18 @@
-
 function PersonajeSkeleton() {
   return (
-    <div className='border border-[#e8a02025] rounded-2xl bg-[#0a1a2e] overflow-hidden animate-pulse w-48'>
-      <div className='w-full h-52 bg-[#111c30]' />
-      <div className='bg-[#0d1829] px-4 py-3 flex flex-col gap-2'>
-        <div className='h-2.5 w-10 bg-[#1a2840] rounded-full' />
-        <div className='h-4 w-28 bg-[#1a2840] rounded-full' />
-        <div className='h-2.5 w-20 bg-[#1a2840] rounded-full' />
+    <div className='border border-[#e8a02025] rounded-2xl bg-[#1E1A1A] overflow-hidden animate-pulse w-48'>
+      
+      {/*barra de raza*/}
+      <div className='w-full h-7 bg-[#2A2020]' />
+
+      {/*area de imagen*/}
+      <div className='w-full h-52 bg-[#241a02]' />
+
+      {/*cuadro de texto*/}
+      <div className='bg-[#000000] px-4 py-3 flex flex-col gap-2'>
+        <div className='h-2.5 w-10 bg-[#2A2020] rounded-full' />
+        <div className='h-4 w-28 bg-[#2A2020] rounded-full' />
+        <div className='h-2.5 w-20 bg-[#2A2020] rounded-full' />
       </div>
     </div>
   )
