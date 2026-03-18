58af4c8d7ce79472a168673cc1339fc43e959f88
JorgeCastroAguirre <JorgeCastroAguirre>
2026-03-18 09:23:11 -0500

    feat: :lipstick: se añadio nuevo estilo a la barra de navegacion

--- src/components/BarraDeBusqueda.jsx
+++ src/components/BarraDeBusqueda.jsx
@@ -10,16 +10,18 @@ function BarraDeBusqueda() {
   return (
     <>
       <div className='bg-[#1E1A1A] border [#e8a020] rounded-[5px] w-100 h-20 flex justify-evenly items-center'>
-        <div className='w-[15%] h-15'>
+        <div className='w-[15%] h-15 ml-2'>
           <img className='w-full h-full object-contain' src={imagen_esfera} alt="esfera"/>
         </div>
+        
         <input 
-          className='bg-white border border-[#4a4a46] rounded-[5px] w-[80%] h-13 text-black' 
+          className='bg-white border border-[#4a4a46] rounded-[5px] w-[80%] h-13 text-black mx-4' 
           type="text" 
-          placeholder='Buscar por nombre, raza, afiliacion...'
+          placeholder='Buscar por nombre ...'
           value={buscar}
           onChange={(e)=>setBuscar(e.target.value)} 
         />
+        
       </div>
     </>
   )
